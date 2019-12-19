2b369e16ee679f30471705dec3130ccdea70b557
Manyang Megantoro <47728231+manyang-megantoro@users.noreply.github.com>
2019-12-19 09:08:05 +0700

    Create Ternary Operator

---
+++ Ternary Operator.php
@@ -0,0 +1,13 @@
+<?php
+//https://jagowebdev.com/cara-penulisan-if-else-pada-php/
+
+//ternary
+$nilai = 9;
+echo $nilai > 5 ? 'Sangat Baik' : 'Buruk';
+//normal
+if ($nilai > 5) {
+	echo 'Sangat Baik';
+} else {
+	echo 'Buruk';
+}
+?>
